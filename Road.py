253e5edf12805a151c9ec5ab962eaa4bffe871b7
hjhjhj <huizhouhust@gmail.com>
2013-09-02 21:22:21 -0500

    Update Road.py

--- Road.py
+++ Road.py
@@ -15,7 +15,7 @@ class SelectGasStation:
             cnt+=1
             if summ < 0:
                 summ=0.0
-                cnt=0
+                #cnt=0
                 result= i+1
             i= i+1 if i+1<n else 1
         return result
