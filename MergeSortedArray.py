5d31ca800e845452c965d53e94170b568aef5b75
Huijun Wu <huijun.wu.2010@gmail.com>
2013-09-29 20:25:47 -0700

    another code
    see history for better code

--- MergeSortedArray.py
+++ MergeSortedArray.py
@@ -1,19 +1,19 @@
 class MergeSortedArray:
     def mergeSortedArray(self, arr1, arr2, n, m):
-        if not m and not n:
-            return
-        
-        i=m-1
-        j=n-1
-        x=n+m-1
-        while i>=0 and j>=0:
-            if arr2[i]>arr1[j]:
-                arr1[x]=arr2[i]
-                i-=1
-            else: 
-                arr1[x]=arr1[j]
-                j-=1
-            x-=1
-            
-        if i>=0:
-            arr1[0:x+1]=arr2[0:i+1]
+        idx1 = n-1
+        idx2 = m-1
+        for i in range(n+m):
+            idx = n+m-1-i
+            if idx1>=0 and idx2>=0:
+                if arr1[idx1] >= arr2[idx2]:
+                    arr1[idx] = arr1[idx1]
+                    idx1 -= 1
+                elif arr1[idx1] < arr2[idx2]:
+                    arr1[idx] = arr2[idx2]
+                    idx2 -= 1
+            elif idx1>=0:
+                arr1[idx] = arr1[idx1]
+                idx1 -= 1
+            elif idx2>=0:
+                arr1[idx] = arr2[idx2]
+                idx2 -= 1
